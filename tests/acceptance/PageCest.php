c741b94daab49f798432f4e840650f834d92e948
Mikhail Gerasimchuk <mgerasimchuk@pushwoosh.com>
2019-05-18 19:19:45 +0700

    add selenoid with live video

--- tests/acceptance/PageCest.php
+++ tests/acceptance/PageCest.php
@@ -2,11 +2,18 @@
 
 class PageCest
 {
+    // TODO надо еще в гитлаб CI достукиваться до кубика продовского и выводить все вресии и все логи в случае ошибки
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
         $I->amOnPage('/metronic/preview/?page=inner&demo=demo9');
-        $I->resizeWindow(400, 700);
+        $I->resizeWindow(1900, 1100);
+        usleep(9000000);
+        $I->amOnUrl("https://google.com");
+
+        usleep(9000000);
+        $I->amOnUrl("https://yandex.ru");
+        usleep(9000000);
         $I->dontSeeVisualChanges('page');
     }
 }
